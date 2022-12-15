e54d345f681ebaf77b459a118a748d062c48e363
Liam Wilson <liam.wilson@kainos.com>
2022-12-15 14:58:05 +0000

    triaging issue

--- app/data/index.js
+++ app/data/index.js
@@ -15,21 +15,7 @@ let sequelize
 try {
   console.log('Attempting to setup sequalize.')
   sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
-    define: {
-      createdAt: 'createdAt',
-      updatedAt: 'updatedAt'
-    },
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: true
-    },
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    logging: function (str) {
-      console.log('inside of sequalize logging function')
-      console.log(str)
-    },
-    schema: process.env.POSTGRES_SCHEMA_NAME
+    logging: console.debug
   })
   console.log('After sequalize constructor')
 } catch (error) {

5a9d03e1cdef34b2710927a2118da0a947eb17f5
Liam Wilson <liam.wilson@kainos.com>
2022-12-15 12:51:32 +0000

    triaging issue

--- app/eligibility/check-eligibility.js
+++ app/eligibility/check-eligibility.js
@@ -1,12 +1,13 @@
-const db = require('../data')
+// const db = require('../data')
 
 const checkEligibility = async (sbi, crn) => {
-  return db.eligibility.findOne({
-    where: {
-      sbi,
-      crn
-    }
-  })
+  // return db.eligibility.findOne({
+  //   where: {
+  //     sbi,
+  //     crn
+  //   }
+  // })
+  return true
 }
 
 module.exports = checkEligibility

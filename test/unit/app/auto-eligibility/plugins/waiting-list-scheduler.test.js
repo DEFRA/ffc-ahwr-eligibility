5a95b9661c98cc6df02f78483ac5b8c08250a83f
Liam Wilson <liam.wilson@kainos.com>
2022-12-19 13:03:18 +0000

    Rebased

--- test/unit/app/auto-eligibility/plugins/waiting-list-scheduler.test.js
+++ test/unit/app/auto-eligibility/plugins/waiting-list-scheduler.test.js
@@ -1,4 +1,5 @@
 describe('Waiting list plugin test', () => {
+<<<<<<< HEAD
   const OLD_ENV = process.env
 
   beforeEach(async () => {
@@ -13,6 +14,9 @@ describe('Waiting list plugin test', () => {
 
   test('test node cron executed', async () => {
     process.env.WAITING_LIST_SCHEDULE = '0 9 * * FRI'
+=======
+  test('test node cron executed', async () => {
+>>>>>>> 2a5e346 (Added cron scheduler for waiting list functionality)
     const mockNodeCron = require('node-cron')
     jest.mock('node-cron', () => {
       return {
@@ -21,6 +25,7 @@ describe('Waiting list plugin test', () => {
     })
     const waitingListScheduler = require('../../../../../app/auto-eligibility/plugins/waiting-list-scheduler')
     await waitingListScheduler.plugin.register()
+<<<<<<< HEAD
     expect(mockNodeCron.schedule).toHaveBeenCalledWith(
       '0 9 * * FRI', expect.any(Function), { scheduled: true }
     )
@@ -41,5 +46,8 @@ describe('Waiting list plugin test', () => {
     await waitingListScheduler.plugin.register()
     expect(mockProcessWaitingList.processWaitingList).toHaveBeenCalledWith(50)
     expect(consoleLogSpy).toHaveBeenCalledWith('Executing process waiting list with limit of 50.')
+=======
+    expect(mockNodeCron.schedule).toHaveBeenCalledTimes(1)
+>>>>>>> 2a5e346 (Added cron scheduler for waiting list functionality)
   })
 })

e54407d05e2eb8d9d5311754a015b047c1888aa9
Marcin Mogiela <marcinmo@kainos.com>
2022-12-21 16:48:38 +0100

    FFC-135605: call db when /api/eligibility

--- app/auto-eligibility/routes/eligibility.js
+++ app/auto-eligibility/routes/eligibility.js
@@ -1,5 +1,6 @@
 const Joi = require('joi')
 const Boom = require('@hapi/boom')
+const eligibilityDbTable = require('../db/eligibility.db.table')
 
 module.exports = {
   method: 'GET',
@@ -20,17 +21,27 @@ module.exports = {
       }
     }
   },
-  handler: (request, h) => {
-    const response = {
-      farmerName: 'David Smith',
-      name: 'David\'s Farm',
-      sbi: '441111114',
-      crn: '4411111144',
-      address: 'Some Road, London, MK55 7ES',
-      email: 'name@email.com'
+  handler: async (request, h) => {
+    try {
+      const farmer = await eligibilityDbTable.findByBusinessEmail(
+        request.query.emailAddress
+      )
+      if (!farmer || !farmer.access_granted) {
+        return Boom.notFound('Farmer not found')
+      }
+      return h
+        .response({
+          farmerName: farmer.customer_name,
+          name: farmer.business_name,
+          sbi: farmer.sbi,
+          crn: farmer.crn,
+          address: farmer.business_address,
+          email: farmer.business_email
+        })
+        .code(200)
+    } catch (error) {
+      console.error(error)
+      throw Boom.internal(error)
     }
-    return h
-      .response(response)
-      .code(200)
   }
 }

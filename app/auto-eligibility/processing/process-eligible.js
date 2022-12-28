18f1f6a558d90b43e1e8bbb7d027ee85c2b5b7f1
Abidemi Adio <abidemi.adio@defra.gov.uk>
2022-12-28 10:43:05 +0000

    test pull

--- app/auto-eligibility/processing/process-eligible.js
+++ app/auto-eligibility/processing/process-eligible.js
@@ -2,7 +2,7 @@ const updateWaiting = require('./update-waiting')
 const emailNotifier = require('../email-notifier')
 
 const processEligible = async (sbi, crn, businessEmailAddress) => {
-  console.log(`Processing as eligible: ${JSON.stringify({ sbi, crn, businessEmailAddress })}`)
+  console.log(`Processing as eligible : ${JSON.stringify({ sbi, crn, businessEmailAddress })}`)
   await updateWaiting(sbi, crn)
   await emailNotifier.sendWaitingListEmail(businessEmailAddress)
 }

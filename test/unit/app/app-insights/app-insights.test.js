ea782898f77fc0555d1c54c23efac8174a0a75d5
Marcin Mogiela <marcinmo@kainos.com>
2023-01-03 14:44:56 +0100

    FFC-141453: lint fix

--- test/unit/app/app-insights/app-insights.test.js
+++ test/unit/app/app-insights/app-insights.test.js
@@ -38,12 +38,12 @@ describe('App Insights', () => {
           appName: 'mock_app_name'
         }
       })
-      appInsightsConfig = require('../../../../app/app-insights/app-insights.config')
+      const appInsightsConfig = require('../../../../app/app-insights/app-insights.config')
       const cloudRoleTag = appInsights.defaultClient.context.keys.cloudRole
-  
+
       const { setup } = require('../../../../app/app-insights/app-insights')
       setup()
-  
+
       expect(MOCK_APP_INSIGHTS_SETUP).toHaveBeenCalledTimes(1)
       expect(logSpy).toHaveBeenCalledWith('App Insights Running')
       expect(appInsights.defaultClient.context.tags[cloudRoleTag]).toEqual(appInsightsConfig.appName)
@@ -58,10 +58,10 @@ describe('App Insights', () => {
           appName: undefined
         }
       })
-  
+
       const { setup } = require('../../../../app/app-insights/app-insights')
       setup()
-  
+
       expect(MOCK_APP_INSIGHTS_SETUP).toHaveBeenCalledTimes(0)
       expect(logSpy).toHaveBeenCalledWith('App Insights Not Running!')
     })
@@ -70,23 +70,22 @@ describe('App Insights', () => {
   describe('logException', () => {
     test('when called with empty request and empty event', () => {
       const { logException } = require('../../../../app/app-insights/app-insights')
-      expect(logException).toBeDefined()
-  
+
       logException({}, {})
-      
+
       expect(appInsights.defaultClient.trackException).toHaveBeenCalledWith({
         exception: new Error('unknown'),
         properties: {
           statusCode: undefined,
           sessionId: undefined,
           payload: undefined,
-          request: 'Server Error',
+          request: 'Server Error'
         }
       })
     })
 
     test('when called with both request and event', () => {
-      let req = {
+      const req = {
         statusCode: 200,
         yar: { id: 'mock_id' },
         payload: 'mock_payload'
@@ -105,7 +104,7 @@ describe('App Insights', () => {
           statusCode: req.statusCode,
           sessionId: req.yar.id,
           payload: req.payload,
-          request: event.request,
+          request: event.request
         }
       })
     })

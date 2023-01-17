cf1394e633a917468ddce7781e487c224f16cda9
Marcin Mogiela <marcinmo@kainos.com>
2023-01-17 09:28:13 +0100

    FFC-145631: added timestamp to nofity send-email logs

--- app/notify/send-email.js
+++ app/notify/send-email.js
@@ -2,12 +2,12 @@ const notifyClient = require('./notify-client')
 
 module.exports = async (templateId, email, options) => {
   try {
-    console.log(`Attempting to send email with template ID ${templateId} to email ${email}`)
+    console.log(`${new Date().toISOString()} Attempting to send email with template ID ${templateId} to email ${email}`)
     await notifyClient.sendEmail(templateId, email, options)
-    console.log(`Successfully sent email with template ID ${templateId} to email ${email}`)
+    console.log(`${new Date().toISOString()} Successfully sent email with template ID ${templateId} to email ${email}`)
     return true
   } catch (e) {
-    console.error(`Error ${JSON.stringify(e.response.data.errors)} occurred during sending of email to address ${email} with template ID ${templateId}.`)
+    console.error(`${new Date().toISOString()} Error ${JSON.stringify(e.response.data.errors)} occurred during sending of email to address ${email} with template ID ${templateId}.`)
     return false
   }
 }

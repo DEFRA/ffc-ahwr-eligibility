568d55f23b885b6916de61b9d9954c27368aeef5
json-hamilton <jason.hamilton@kainos.com>
2023-01-31 16:31:10 +0000

    FFC-148185: Feature Toggle for Checking Emails are linked to Multiple SBI

--- app/auto-eligibility/config/index.js
+++ app/auto-eligibility/config/index.js
@@ -6,7 +6,8 @@ const schema = Joi.object({
     enabled: Joi.bool().default(true),
     schedule: Joi.string().default('0 9 * * TUE'), // At 09:00 AM, only on Tuesday
     upperLimit: Joi.number().default(50)
-  }
+  },
+  checkEmailLinkedToMultipleSbiEnabled: Joi.bool().default(true)
 })
 
 const config = {
@@ -15,7 +16,8 @@ const config = {
     enabled: process.env.WAITING_LIST_SCHEDULER_ENABLED,
     schedule: process.env.WAITING_LIST_SCHEDULE,
     upperLimit: process.env.WAITING_LIST_THRESHOLD_UPPER_LIMIT
-  }
+  },
+  checkEmailLinkedToMultipleSbiEnabled: process.env.CHECK_EMAIL_LINKED_TO_MULTIPLE_SBI_ENABLED
 }
 
 const { error, value } = schema.validate(config, { abortEarly: false })
--- app/auto-eligibility/register-your-interest/process-eligible-sbi.js
+++ app/auto-eligibility/register-your-interest/process-eligible-sbi.js
@@ -5,7 +5,9 @@ const processEligibleSbi = async (customer) => {
   console.log(`${new Date().toISOString()} Processing eligible SBI: ${JSON.stringify({
      ...customer
   })}`)
-  if (customer.businessEmailHasMultipleDistinctSbi()) {
+  console.log(customer.businessEmailHasMultipleDistinctSbi())
+  console.log(process.env.CHECK_EMAIL_LINKED_TO_MULTIPLE_SBI_ENABLED)
+  if (customer.businessEmailHasMultipleDistinctSbi() && process.env.CHECK_EMAIL_LINKED_TO_MULTIPLE_SBI_ENABLED == true) {
     console.log(`${new Date().toISOString()} The customer's business email has multiple distinct SBI`)
     return await emailNotifier.sendIneligibleApplicationEmail(
       customer.sbi,

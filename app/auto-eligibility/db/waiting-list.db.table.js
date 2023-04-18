f3060dc8ed49fdd30e483a454b3fad1135fc0805
Jason Hamilton <115159962+json-hamilton@users.noreply.github.com>
2023-04-18 09:11:05 +0100

    FFC-170883: Waiting List Scheduler (#53)
    * Waiting List scheduler updated
    
    * Added process waiting list test
    
    * Added tests
    
    * Made tests pass

--- app/auto-eligibility/db/waiting-list.db.table.js
+++ app/auto-eligibility/db/waiting-list.db.table.js
@@ -1,4 +1,4 @@
-const { fn, col, where } = require('sequelize')
+const { fn, col, where, QueryTypes } = require('sequelize')
 const db = require('../../data')
 
 const findAllByBusinessEmail = async (businessEmail) => {
@@ -30,7 +30,41 @@ const registerInterest = async (businessEmail) => {
   return result
 }
 
+const updateAccessGranted = async (upperLimit) => {
+  console.log(`${new Date().toISOString()} Updating access granted: ${JSON.stringify({ upperLimit })}`)
+  if (typeof upperLimit === 'undefined') {
+    throw new Error(`Invalid argument: ${JSON.stringify(upperLimit)}`)
+  }
+  const result = await db.sequelize.query(`
+      UPDATE waiting_list 
+      SET access_granted = true, access_granted_at = now()
+      FROM (
+        SELECT business_email
+        FROM waiting_list
+        WHERE access_granted_at IS NULL
+          AND access_granted = false
+        ORDER BY access_granted_at ASC LIMIT :limit
+      ) c 
+      WHERE waiting_list.business_email = c.business_email
+      RETURNING
+        waiting_list.business_email,
+        waiting_list.created_at,
+        waiting_list.access_granted,
+        waiting_list.access_granted_at`,
+  {
+    replacements: { limit: upperLimit },
+    type: QueryTypes.UPDATE
+  })
+  return result[0].map(customer => ({
+    businessEmail: customer.business_email,
+    createdAt: customer.created_at,
+    accessGranted: customer.access_granted,
+    accessGrantedAt: customer.access_granted_at
+  }))
+}
+
 module.exports = {
   findAllByBusinessEmail,
-  registerInterest
+  registerInterest,
+  updateAccessGranted
 }
--- test/unit/auto-eligibility/waiting-list/process-waiting-list.test.js
+++ test/unit/auto-eligibility/waiting-list/process-waiting-list.test.js
@@ -4,136 +4,295 @@ const telemetryEvent = require('../../../../app/auto-eligibility/telemetry/telem
 const MOCK_NOW = new Date()
 
 describe('Process waiting list function test.', () => {
-  let db
-  const MOCK_SEND_EVENT = jest.fn()
+  describe('When defraId is disabled', () => {
+    let db
+    const MOCK_SEND_EVENT = jest.fn()
 
-  beforeAll(() => {
-    jest.useFakeTimers('modern')
-    jest.setSystemTime(MOCK_NOW)
+    beforeAll(() => {
+      jest.useFakeTimers('modern')
+      jest.setSystemTime(MOCK_NOW)
 
-    jest.mock('../../../../app/app-insights/app-insights.config', () => ({
-      appInsightsCloudRole: 'mock_app_insights_cloud_role'
-    }))
+      jest.mock('../../../../app/app-insights/app-insights.config', () => ({
+        appInsightsCloudRole: 'mock_app_insights_cloud_role'
+      }))
 
-    jest.mock('../../../../app/data')
-    db = require('../../../../app/data')
+      jest.mock('../../../../app/data')
+      db = require('../../../../app/data')
 
-    jest.mock('ffc-ahwr-event-publisher', () => {
-      return {
-        PublishEvent: jest.fn().mockImplementation(() => {
-          return {
-            sendEvent: MOCK_SEND_EVENT
-          }
-        })
-      }
-    })
-  })
-
-  afterAll(() => {
-    jest.resetModules()
-    jest.useRealTimers()
-  })
-
-  afterEach(() => {
-    resetAllWhenMocks()
-    jest.resetAllMocks()
-  })
-
-  test('test mulitple records updated', async () => {
-    const spyConsole = jest.spyOn(console, 'log')
-    jest.mock('../../../../app/auto-eligibility/email-notifier', () => {
-      return {
-        sendApplyGuidanceEmail: jest.fn()
-      }
-    })
-    const mockEmailNotifier = require('../../../../app/auto-eligibility/email-notifier')
-    when(db.sequelize.query)
-      .calledWith(expect.anything(), expect.anything())
-      .mockResolvedValue([
-        [
-          {
-            sbi: 'mock_sbi',
-            crn: 'mock_crn',
-            business_email: 'test@email.com',
-            waiting_updated_at: MOCK_NOW,
-            access_granted: true,
-            last_updated_at: MOCK_NOW
-          },
-          { business_email: 'test2@email.com' }, { business_email: 'test3@email.com' }], '3'
-      ])
-    const processWaitingList = require('../../../../app/auto-eligibility/waiting-list/process-waiting-list')
-    await processWaitingList(50)
-    expect(spyConsole).toHaveBeenCalledWith(`${MOCK_NOW.toISOString()} auto-eligibility:waiting-list Processing waiting list: ${JSON.stringify({ upperLimit: 50 })}`)
-    expect(spyConsole).toHaveBeenCalledWith(`${MOCK_NOW.toISOString()} auto-eligibility:waiting-list [3] new customers are now eligible to apply for a review`)
-    expect(db.sequelize.query).toHaveBeenCalledTimes(1)
-    expect(mockEmailNotifier.sendApplyGuidanceEmail).toHaveBeenCalledTimes(3)
-    expect(MOCK_SEND_EVENT).toHaveBeenCalledTimes(3)
-    expect(MOCK_SEND_EVENT).toHaveBeenNthCalledWith(1, {
-      name: 'send-session-event',
-      properties: {
-        id: 'mock_sbi_mock_crn',
-        sbi: 'mock_sbi',
-        cph: 'n/a',
-        checkpoint: 'mock_app_insights_cloud_role',
-        status: 'success',
-        action: {
-          type: `auto-eligibility:${telemetryEvent.GAINED_ACCESS_TO_THE_APPLY_JOURNEY}`,
-          message: 'The user has gained access to the apply journey',
-          data: {
-            crn: 'mock_crn',
-            sbi: 'mock_sbi',
-            businessEmail: 'test@email.com',
-            onWaitingList: false,
-            waitingUpdatedAt: MOCK_NOW,
-            eligible: true,
-            ineligibleReason: 'n/a',
-            accessGranted: true,
-            accessGrantedAt: MOCK_NOW
-          },
-          raisedBy: 'test@email.com'
+      jest.mock('ffc-ahwr-event-publisher', () => {
+        return {
+          PublishEvent: jest.fn().mockImplementation(() => {
+            return {
+              sendEvent: MOCK_SEND_EVENT
+            }
+          })
         }
-      }
-    })
-  })
+      })
 
-  test('test no records updated', async () => {
-    const spyConsole = jest.spyOn(console, 'log')
-    jest.mock('../../../../app/auto-eligibility/email-notifier', () => {
-      return {
-        sendApplyGuidanceEmail: jest.fn()
-      }
+      jest.mock('../../../../app/auto-eligibility/config', () => ({
+        defraId: {
+          enabled: false
+        }
+      }))
     })
-    const mockEmailNotifier = require('../../../../app/auto-eligibility/email-notifier')
-    when(db.sequelize.query)
-      .calledWith(expect.anything(), expect.anything())
-      .mockResolvedValue([
-        [], '0'
-      ])
-    const processWaitingList = require('../../../../app/auto-eligibility/waiting-list/process-waiting-list')
-    await processWaitingList(50)
-    expect(spyConsole).toHaveBeenCalledWith(`${MOCK_NOW.toISOString()} auto-eligibility:waiting-list Processing waiting list: ${JSON.stringify({ upperLimit: 50 })}`)
-    expect(spyConsole).toHaveBeenCalledWith(`${MOCK_NOW.toISOString()} auto-eligibility:waiting-list [0] new customers are now eligible to apply for a review`)
-    expect(db.sequelize.query).toHaveBeenCalledTimes(1)
-    expect(mockEmailNotifier.sendApplyGuidanceEmail).not.toHaveBeenCalled()
-    expect(MOCK_SEND_EVENT).not.toHaveBeenCalled()
-  })
 
-  test('test error handled', async () => {
-    jest.mock('../../../../app/auto-eligibility/email-notifier', () => {
-      return {
-        sendApplyGuidanceEmail: jest.fn()
-      }
+    afterAll(() => {
+      jest.resetModules()
+      jest.useRealTimers()
     })
-    const mockEmailNotifier = require('../../../../app/auto-eligibility/email-notifier')
-    when(db.sequelize.query)
-      .calledWith(expect.anything(), expect.anything())
-      .mockRejectedValue(new Error('Some DB error'))
-    const processWaitingList = require('../../../../app/auto-eligibility/waiting-list/process-waiting-list')
-    expect(async () =>
+
+    afterEach(() => {
+      resetAllWhenMocks()
+      jest.resetAllMocks()
+    })
+
+    test('test mulitple records updated', async () => {
+      const spyConsole = jest.spyOn(console, 'log')
+      jest.mock('../../../../app/auto-eligibility/email-notifier', () => {
+        return {
+          sendApplyGuidanceEmail: jest.fn()
+        }
+      })
+      const mockEmailNotifier = require('../../../../app/auto-eligibility/email-notifier')
+      when(db.sequelize.query)
+        .calledWith(expect.anything(), expect.anything())
+        .mockResolvedValue([
+          [
+            {
+              sbi: 'mock_sbi',
+              crn: 'mock_crn',
+              business_email: 'test@email.com',
+              waiting_updated_at: MOCK_NOW,
+              access_granted: true,
+              last_updated_at: MOCK_NOW
+            },
+            { business_email: 'test2@email.com' }, { business_email: 'test3@email.com' }], '3'
+        ])
+      const processWaitingList = require('../../../../app/auto-eligibility/waiting-list/process-waiting-list')
       await processWaitingList(50)
-    ).rejects.toThrowError('Some DB error')
-    expect(db.sequelize.query).toHaveBeenCalledTimes(1)
-    expect(mockEmailNotifier.sendApplyGuidanceEmail).not.toHaveBeenCalled()
-    expect(MOCK_SEND_EVENT).not.toHaveBeenCalled()
+      expect(spyConsole).toHaveBeenCalledWith(`${MOCK_NOW.toISOString()} auto-eligibility:waiting-list Processing waiting list: ${JSON.stringify({ upperLimit: 50 })}`)
+      expect(spyConsole).toHaveBeenCalledWith(`${MOCK_NOW.toISOString()} auto-eligibility:waiting-list [3] new customers are now eligible to apply for a review`)
+      expect(db.sequelize.query).toHaveBeenCalledTimes(1)
+      expect(mockEmailNotifier.sendApplyGuidanceEmail).toHaveBeenCalledTimes(3)
+      expect(MOCK_SEND_EVENT).toHaveBeenCalledTimes(3)
+      expect(MOCK_SEND_EVENT).toHaveBeenNthCalledWith(1, {
+        name: 'send-session-event',
+        properties: {
+          id: 'mock_sbi_mock_crn',
+          sbi: 'mock_sbi',
+          cph: 'n/a',
+          checkpoint: 'mock_app_insights_cloud_role',
+          status: 'success',
+          action: {
+            type: `auto-eligibility:${telemetryEvent.GAINED_ACCESS_TO_THE_APPLY_JOURNEY}`,
+            message: 'The user has gained access to the apply journey',
+            data: {
+              crn: 'mock_crn',
+              sbi: 'mock_sbi',
+              businessEmail: 'test@email.com',
+              onWaitingList: false,
+              waitingUpdatedAt: MOCK_NOW,
+              eligible: true,
+              ineligibleReason: 'n/a',
+              accessGranted: true,
+              accessGrantedAt: MOCK_NOW
+            },
+            raisedBy: 'test@email.com'
+          }
+        }
+      })
+    })
+
+    test('test no records updated', async () => {
+      const spyConsole = jest.spyOn(console, 'log')
+      jest.mock('../../../../app/auto-eligibility/email-notifier', () => {
+        return {
+          sendApplyGuidanceEmail: jest.fn()
+        }
+      })
+      const mockEmailNotifier = require('../../../../app/auto-eligibility/email-notifier')
+      when(db.sequelize.query)
+        .calledWith(expect.anything(), expect.anything())
+        .mockResolvedValue([
+          [], '0'
+        ])
+      const processWaitingList = require('../../../../app/auto-eligibility/waiting-list/process-waiting-list')
+      await processWaitingList(50)
+      expect(spyConsole).toHaveBeenCalledWith(`${MOCK_NOW.toISOString()} auto-eligibility:waiting-list Processing waiting list: ${JSON.stringify({ upperLimit: 50 })}`)
+      expect(spyConsole).toHaveBeenCalledWith(`${MOCK_NOW.toISOString()} auto-eligibility:waiting-list [0] new customers are now eligible to apply for a review`)
+      expect(db.sequelize.query).toHaveBeenCalledTimes(1)
+      expect(mockEmailNotifier.sendApplyGuidanceEmail).not.toHaveBeenCalled()
+      expect(MOCK_SEND_EVENT).not.toHaveBeenCalled()
+    })
+
+    test('test error handled', async () => {
+      jest.mock('../../../../app/auto-eligibility/email-notifier', () => {
+        return {
+          sendApplyGuidanceEmail: jest.fn()
+        }
+      })
+      const mockEmailNotifier = require('../../../../app/auto-eligibility/email-notifier')
+      when(db.sequelize.query)
+        .calledWith(expect.anything(), expect.anything())
+        .mockRejectedValue(new Error('Some DB error'))
+      const processWaitingList = require('../../../../app/auto-eligibility/waiting-list/process-waiting-list')
+      expect(async () =>
+        await processWaitingList(50)
+      ).rejects.toThrowError('Some DB error')
+      expect(db.sequelize.query).toHaveBeenCalledTimes(1)
+      expect(mockEmailNotifier.sendApplyGuidanceEmail).not.toHaveBeenCalled()
+      expect(MOCK_SEND_EVENT).not.toHaveBeenCalled()
+    })
+  })
+  describe('When defraId is enabled', () => {
+    let db
+    const MOCK_SEND_EVENT = jest.fn()
+
+    beforeAll(() => {
+      jest.useFakeTimers('modern')
+      jest.setSystemTime(MOCK_NOW)
+
+      jest.mock('../../../../app/app-insights/app-insights.config', () => ({
+        appInsightsCloudRole: 'mock_app_insights_cloud_role'
+      }))
+
+      jest.mock('../../../../app/data')
+      db = require('../../../../app/data')
+
+      jest.mock('ffc-ahwr-event-publisher', () => {
+        return {
+          PublishEvent: jest.fn().mockImplementation(() => {
+            return {
+              sendEvent: MOCK_SEND_EVENT
+            }
+          })
+        }
+      })
+
+      jest.mock('../../../../app/auto-eligibility/config', () => ({
+        defraId: {
+          enabled: true
+        }
+      }))
+    })
+
+    afterAll(() => {
+      jest.resetModules()
+      jest.useRealTimers()
+    })
+
+    afterEach(() => {
+      resetAllWhenMocks()
+      jest.resetAllMocks()
+    })
+
+    test('test mulitple records updated', async () => {
+      const spyConsole = jest.spyOn(console, 'log')
+      jest.mock('../../../../app/auto-eligibility/email-notifier', () => {
+        return {
+          sendApplyGuidanceEmail: jest.fn()
+        }
+      })
+      const mockEmailNotifier = require('../../../../app/auto-eligibility/email-notifier')
+      when(db.sequelize.query)
+        .calledWith(expect.anything(), expect.anything())
+        .mockResolvedValue([
+          [
+            {
+              business_email: 'test@email.com',
+              created_at: MOCK_NOW,
+              access_granted: true,
+              access_granted_at: MOCK_NOW
+            },
+            { business_email: 'test2@email.com' }, { business_email: 'test3@email.com' }], '3'
+        ])
+      const processWaitingList = require('../../../../app/auto-eligibility/waiting-list/process-waiting-list')
+      await processWaitingList(50)
+      expect(spyConsole).toHaveBeenCalledWith(`${MOCK_NOW.toISOString()} auto-eligibility:waiting-list Processing waiting list: ${JSON.stringify({ upperLimit: 50 })}`)
+      expect(spyConsole).toHaveBeenCalledWith(`${MOCK_NOW.toISOString()} auto-eligibility:waiting-list [3] new customers are now eligible to apply for a review`)
+      expect(db.sequelize.query).toHaveBeenCalledTimes(1)
+      expect(mockEmailNotifier.sendApplyGuidanceEmail).toHaveBeenCalledTimes(3)
+    })
+
+    test('test one record updated', async () => {
+      const spyConsole = jest.spyOn(console, 'log')
+      jest.mock('../../../../app/auto-eligibility/email-notifier', () => {
+        return {
+          sendApplyGuidanceEmail: jest.fn()
+        }
+      })
+      const mockEmailNotifier = require('../../../../app/auto-eligibility/email-notifier')
+      when(db.sequelize.query)
+        .calledWith(expect.anything(), expect.anything())
+        .mockResolvedValue([
+          [
+            {
+              business_email: 'test@email.com',
+              created_at: MOCK_NOW,
+              access_granted: true,
+              access_granted_at: MOCK_NOW
+            }], '1'
+        ])
+      const processWaitingList = require('../../../../app/auto-eligibility/waiting-list/process-waiting-list')
+      await processWaitingList(50)
+      expect(spyConsole).toHaveBeenCalledWith(`${MOCK_NOW.toISOString()} auto-eligibility:waiting-list Processing waiting list: ${JSON.stringify({ upperLimit: 50 })}`)
+      expect(spyConsole).toHaveBeenCalledWith(`${MOCK_NOW.toISOString()} auto-eligibility:waiting-list [1] new customer are now eligible to apply for a review`)
+      expect(db.sequelize.query).toHaveBeenCalledTimes(1)
+      expect(mockEmailNotifier.sendApplyGuidanceEmail).toHaveBeenCalledTimes(1)
+    })
+
+    test('test no records updated', async () => {
+      const spyConsole = jest.spyOn(console, 'log')
+      jest.mock('../../../../app/auto-eligibility/email-notifier', () => {
+        return {
+          sendApplyGuidanceEmail: jest.fn()
+        }
+      })
+      const mockEmailNotifier = require('../../../../app/auto-eligibility/email-notifier')
+      when(db.sequelize.query)
+        .calledWith(expect.anything(), expect.anything())
+        .mockResolvedValue([
+          [], '0'
+        ])
+      const processWaitingList = require('../../../../app/auto-eligibility/waiting-list/process-waiting-list')
+      await processWaitingList(50)
+      expect(spyConsole).toHaveBeenCalledWith(`${MOCK_NOW.toISOString()} auto-eligibility:waiting-list Processing waiting list: ${JSON.stringify({ upperLimit: 50 })}`)
+      expect(spyConsole).toHaveBeenCalledWith(`${MOCK_NOW.toISOString()} auto-eligibility:waiting-list [0] new customers are now eligible to apply for a review`)
+      expect(db.sequelize.query).toHaveBeenCalledTimes(1)
+      expect(mockEmailNotifier.sendApplyGuidanceEmail).not.toHaveBeenCalled()
+    })
+
+    test('test upper limit not defined', async () => {
+      jest.mock('../../../../app/auto-eligibility/email-notifier', () => {
+        return {
+          sendApplyGuidanceEmail: jest.fn()
+        }
+      })
+      const mockEmailNotifier = require('../../../../app/auto-eligibility/email-notifier')
+      const processWaitingList = require('../../../../app/auto-eligibility/waiting-list/process-waiting-list')
+      expect(async () =>
+        await processWaitingList(undefined)
+      ).rejects.toThrowError('Invalid argument: undefined')
+      expect(db.sequelize.query).not.toHaveBeenCalled()
+      expect(mockEmailNotifier.sendApplyGuidanceEmail).not.toHaveBeenCalled()
+    })
+
+    test('test error handled', async () => {
+      jest.mock('../../../../app/auto-eligibility/email-notifier', () => {
+        return {
+          sendApplyGuidanceEmail: jest.fn()
+        }
+      })
+      const mockEmailNotifier = require('../../../../app/auto-eligibility/email-notifier')
+      when(db.sequelize.query)
+        .calledWith(expect.anything(), expect.anything())
+        .mockRejectedValue(new Error('Some DB error'))
+      const processWaitingList = require('../../../../app/auto-eligibility/waiting-list/process-waiting-list')
+      expect(async () =>
+        await processWaitingList(50)
+      ).rejects.toThrowError('Some DB error')
+      expect(db.sequelize.query).toHaveBeenCalledTimes(1)
+      expect(mockEmailNotifier.sendApplyGuidanceEmail).not.toHaveBeenCalled()
+    })
   })
 })
